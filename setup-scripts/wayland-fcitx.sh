4c64eff7b65d618757ee4d0205095164045fc463
onohiroko <azazabc123@gmail.com>
2019-06-25 11:14:46 +0800

    wayland....

---
+++ setup-scripts/wayland-fcitx.sh
@@ -0,0 +1,5 @@
+echo Append following lines to /etc/environment:
+echo ======================================================
+echo GTK_IM_MODULE=fcitx
+echo QT_IM_MODULE=fcitx
+echo XMODIFIERS=@im=fcitx
